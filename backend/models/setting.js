564693087ac95514584b01ec9e917b230b6e8192
alwey <alwey64@gmail.com>
2025-05-16 15:24:16 +0700

    fix: [ah] setting model

--- backend/models/setting.js
+++ backend/models/setting.js
@@ -11,31 +11,8 @@ module.exports = (sequelize) => {
       type: DataTypes.TEXT,
       allowNull: true,
     },
-
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
-    role: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
-    status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isIn: [['active', 'non-active']],
-      },
-    },
-
-    photo: {
-      type: DataTypes.TEXT,  // TEXT is better for Base64 than STRING
-      allowNull: true,
-      defaultValue: null,
-    },
-  }, {
+  }, 
+  {
     tableName: 'settings',
     timestamps: true
   });
