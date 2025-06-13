efe2dad8d0906a554aaab29a764706092fd786cb
alwey <alwey64@gmail.com>
2025-06-14 00:23:32 +0700

    feat: [ah] implement product page

--- frontend/src/component/cardproduk.js
+++ frontend/src/component/cardproduk.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FaShoppingBag } from "react-icons/fa";
 
-export default function CardProduk({ id, gambar, nama, harga, detail, onBeli }) {
+export default function CardProduk({ id, gambar, nama, harga, onBeli }) {
   return (
     <div className="bg-[#112e42] w-full p-4 rounded-xl shadow-lg flex flex-col items-start sm:max-w-md mx-auto">
       <img
@@ -11,12 +11,12 @@ export default function CardProduk({ id, gambar, nama, harga, detail, onBeli }) 
         className="w-full h-52 object-cover rounded-lg"
       />
       <h3 className="mt-4 text-xl font-semibold text-white">{nama}</h3>
-      <p className="text-base text-white">{harga}</p>
+      <p className="text-base text-white">Rp. {harga.toLocaleString("id-ID")}</p>
       <Link
         to={`/guest/produkdetail/${id}`}
         className="text-sm text-white mt-1"
       >
-        {detail}
+        <p className="hover:underline text-base text-white">Lihat Detail</p>
       </Link>
         <button
           onClick={onBeli}
